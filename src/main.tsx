841619e490f3c1ed5150b48dc3fc8c3f84dcf58f
murrayee <murrayee@163.com>
2025-04-10 22:35:10 +0800

    [jiaxin] feat: fix approve text

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import Providers from '@/Providers';
 import './index.css';
+import '@fontsource-variable/outfit';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<Providers />);
